10f77b42bedc9372635d873554f4dd81f584abb0
Rodrigo Reyes <kid.alien@gmail.com>
2020-01-04 15:27:23 -0500

    VUEX started wpw

---
+++ section-17-vuex/src/components/AnotherResult.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Counter is: {{ counter }}</p>
+    <p>Number for clicks {{clicks}}</p>
+  </div>
+</template>
+
+<script>
+// This creates computed properties from all the getters
+
+import { mapGetters } from "vuex";
+export default {
+  //this can take  an object and we can remap the getters to new names
+  // propertyName: newName??
+  //   We can also use the ... spread operator pero solo en ES6
+  computed: {
+    //   we need the spread to be able to add our computed properties
+    ...mapGetters({
+      counter: "doubleCounter",
+      clicks: "stringCounter"
+    })
+  }
+};
+</script>
