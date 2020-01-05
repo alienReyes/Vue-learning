15ed871e221572b09d5729983b7e5f04f4911fe5
Rodrigo Reyes <kid.alien@gmail.com>
2020-01-05 14:46:55 -0500

    more nuxt

---
+++ section-17-vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="asyncIncrement({by:100, duration:500})">Increment Async</button>
+    <button class="btn btn-primary" @click="asyncDecrement({by:100, duration:500})">Decrement Async</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  methods: {
+    ...mapActions(["asyncIncrement", "asyncDecrement"])
+  }
+};
+</script>
--- section-17-vuex/src/store/store.js
+++ section-17-vuex/src/store/store.js
@@ -6,12 +6,45 @@ export const store= new Vuex.Store({
    state:{
        counter:0
     },
+    // getters can be seen as Computed properties
     getters:{
         doubleCounter: state=>{
             return state.counter*2;
         },
         stringCounter:state =>{
             return state.counter + ' Clicks'
+        }
+    },
+    // these behave like setters and have to be commited, they are reactive
+    // mutations have to be syncrhonos we have to investigate that
+    mutations:{
+        increment:(state,payload)=>{
+            state.counter+=payload;
+        },
+        decrement:(state,payload)=>{
+            state.counter-=payload;
+        }
+    },
+    // since Mutations run synchronously we have to use ACTIOns that run asynch
+    // this is in case we are waiting  something from the server to update the store
+    // or we have an operation that may take time
+    actions:{
+        // the commit here comes from CONTEXT 
+        increment:({commit},payload)=>{
+            commit('increment',payload);
+        },
+        decrement:({commit},payload)=>{
+            commit('decrement',payload);
+        },
+        asyncIncrement:({commit},payload)=>{
+            setTimeout(()=>{
+                commit('increment',payload.by)
+            },payload.duration)
+        },
+        asyncDecrement:({commit},payload)=>{
+            setTimeout(()=>{
+                commit('decrement',payload.by)
+            },payload.duration)
         }
     }
 });
